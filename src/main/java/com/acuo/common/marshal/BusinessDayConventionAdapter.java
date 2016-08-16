5cc6a440b54d27750f695b7e4800397911383226
Hicham Medkouri <hicham@anaxo.io>
2016-08-16 13:02:47 +0100

    Added static builder methods to the proxy

--- src/main/java/com/acuo/common/marshal/BusinessDayConventionAdapter.java
+++ src/main/java/com/acuo/common/marshal/BusinessDayConventionAdapter.java
@@ -1,17 +1,18 @@
 package com.acuo.common.marshal;
 
+import com.acuo.common.model.BusinessDayConventionProxy;
 import com.opengamma.strata.basics.date.BusinessDayConvention;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public class BusinessDayConventionAdapter extends XmlAdapter<String, BusinessDayConvention> {
+public class BusinessDayConventionAdapter extends XmlAdapter<String, BusinessDayConventionProxy> {
     @Override
-    public BusinessDayConvention unmarshal(String value) throws Exception {
-        return BusinessDayConvention.of(value);
+    public BusinessDayConventionProxy unmarshal(String value) throws Exception {
+        return BusinessDayConventionProxy.of(value);
     }
 
     @Override
-    public String marshal(BusinessDayConvention value) throws Exception {
+    public String marshal(BusinessDayConventionProxy value) throws Exception {
         return value.getName();
     }
 }
--- src/main/java/com/acuo/common/marshal/DayCountAdapter.java
+++ src/main/java/com/acuo/common/marshal/DayCountAdapter.java
@@ -1,17 +1,18 @@
 package com.acuo.common.marshal;
 
+import com.acuo.common.model.DayCountProxy;
 import com.opengamma.strata.basics.date.DayCount;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public class DayCountAdapter extends XmlAdapter<String, DayCount> {
+public class DayCountAdapter extends XmlAdapter<String, DayCountProxy> {
     @Override
-    public DayCount unmarshal(String value) throws Exception {
-        return DayCount.of(value);
+    public DayCountProxy unmarshal(String value) throws Exception {
+        return DayCountProxy.of(value);
     }
 
     @Override
-    public String marshal(DayCount value) throws Exception {
+    public String marshal(DayCountProxy value) throws Exception {
         return value.getName();
     }
 }
