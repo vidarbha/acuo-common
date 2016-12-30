31c993440f4a673f676b6ef35d2dfc7619889cd9
Hicham Medkouri <hicham.medkouri@gmail.com>
2016-12-30 13:47:47 +0800

    OW-239 moved some json utils method in the common package

---
+++ src/main/java/com/acuo/common/json/ValueFormatter.java
@@ -0,0 +1,24 @@
+package com.acuo.common.json;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class ValueFormatter {
+
+    private ValueFormatter() {
+
+    }
+
+    public static String format(Double value) {
+        if (value == null)
+            value = 0.0d;
+        return String.format("%.2f", value);
+    }
+
+    public static String format(LocalDate date) {
+        if (date == null)
+            return "";
+        DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
+        return formatter.format(date);
+    }
+}
