897772ade944b8d13a143744cbce03fb37e24d33
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-01-06 17:06:22 +0800

    OW-320 fixed the dashboard

--- src/main/java/com/acuo/common/json/ValueFormatter.java
+++ src/main/java/com/acuo/common/json/ValueFormatter.java
@@ -1,6 +1,7 @@
 package com.acuo.common.json;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ValueFormatter {
@@ -20,5 +21,12 @@ public class ValueFormatter {
             return "";
         DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
         return formatter.format(date);
+    }
+
+    public static String format(LocalDateTime dateTime) {
+        if (dateTime == null)
+            return "";
+        DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
+        return formatter.format(dateTime);
     }
 }
