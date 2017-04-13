c1f278b57888fee6be0286eb43e75f1599a08d1c
Hicham Medkouri <hicham@anaxo.io>
2017-04-13 07:59:35 +0100

    OW-558 added guice configuration for the calendar service

--- src/test/java/com/acuo/common/calendar/CalendarServiceTest.java
+++ src/test/java/com/acuo/common/calendar/CalendarServiceTest.java
@@ -8,15 +8,26 @@ import org.junit.runner.RunWith;
 import javax.inject.Inject;
 import java.time.LocalDate;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(GuiceJUnitRunner.class)
+@GuiceJUnitRunner.GuiceModules({
+        CalendarModule.class
+})
 @Slf4j
 public class CalendarServiceTest {
 
-//    CalendarService calendarService;
-//
-//    @Test
-//    public void testIsWorkingDay()
-//    {
-//
-//        calendarService.isWorkingDay("FRA", LocalDate.of(2017,07,12));
-//    }
+    @Inject
+    CalendarService calendarService;
+
+    @Test
+    public void testIsWorkingDay(){
+        final LocalDate workingDay = LocalDate.of(2017, 07, 12);
+        final LocalDate holiday = LocalDate.of(2017, 07, 14);
+        boolean isWorkingDay = calendarService.isWorkingDay("FRA", workingDay);
+        assertTrue(isWorkingDay);
+        isWorkingDay = calendarService.isWorkingDay("FRA", holiday);
+        assertFalse(isWorkingDay);
+    }
 }
