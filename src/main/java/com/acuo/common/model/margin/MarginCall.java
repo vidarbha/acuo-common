c043e69c11ff22cf9bc729c47a7e7e360e1fd0fa
lucielerolland <lucie.le.rolland@gmail.com>
2017-04-13 13:32:34 +0800

    OW-123 Added properties to the MarginCall object

--- src/main/java/com/acuo/common/model/margin/MarginCall.java
+++ src/main/java/com/acuo/common/model/margin/MarginCall.java
@@ -14,6 +14,7 @@ import static com.acuo.common.model.margin.Types.*;
 @Data
 public class MarginCall {
 
+    private String id;
     private String ampId;
     private BusinessState businessState;
     private String direction;
