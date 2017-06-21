4ff69deddc1dd0970d3590d5d88239e768a4aa64
lucielerolland <lucie.le.rolland@gmail.com>
2017-06-21 15:52:20 +0800

    OW-509 OW-609 OW-569 Transformed ReutersError into an Error + a set of ReutersMessage

--- src/main/java/com/acuo/common/model/results/ReutersError.java
+++ src/main/java/com/acuo/common/model/results/ReutersError.java
@@ -2,8 +2,20 @@ package com.acuo.common.model.results;
 
 import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 @Data
 public class ReutersError extends Error {
+    private Set<ReutersMessage> reutersMessages = new HashSet<>();
+    public Set<ReutersMessage> getReutersMessages() { return reutersMessages; }
+    public void addReutersMessage(ReutersMessage reutersMessage) { this.reutersMessages.add(reutersMessage); }
+    public void removeAllReutersMessages() {reutersMessages.clear(); }
+}
+
+@Data
+public class ReutersMessage {
     private String errorType;
     private String errorMessage;
 }
