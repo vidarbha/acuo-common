54fd3f0ba93de8b012fc7ce2225ba571c73038df
lucielerolland <lucie.le.rolland@gmail.com>
2017-08-28 11:04:31 +0800

    OW-497 Added @Data to InterestBenchmark

--- src/main/java/com/acuo/common/model/agreements/InterestBenchmark.java
+++ src/main/java/com/acuo/common/model/agreements/InterestBenchmark.java
@@ -1,7 +1,9 @@
 package com.acuo.common.model.agreements;
 
 import com.opengamma.strata.basics.currency.Currency;
+import lombok.Data;
 
+@Data
 public class InterestBenchmark {
     private String benchmarkName;
     private Currency currency;
