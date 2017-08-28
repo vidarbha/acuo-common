83943515b34f5c612dc0ee713f00417e955fc0e2
lucielerolland <lucie.le.rolland@gmail.com>
2017-08-28 16:06:50 +0800

    OW-800 Added TradingParty concept

---
+++ src/main/java/com/acuo/common/model/agreements/TradingParty.java
@@ -0,0 +1,12 @@
+package com.acuo.common.model.agreements;
+
+/**
+ * Not to be confused by our ACUO TradingAccount notion : this is the layer between Organisation
+ * and LegalEntity
+ */
+public class TradingParty {
+
+    private String ampId;
+    private String name;
+
+}
