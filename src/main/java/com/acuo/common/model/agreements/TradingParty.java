e9042ea74c3505d8b6792907dbc923a862fbaad1
lucielerolland <lucie.le.rolland@gmail.com>
2017-08-29 10:51:06 +0800

    OW-800 Added a WhitelistInfo object

--- src/main/java/com/acuo/common/model/agreements/TradingParty.java
+++ src/main/java/com/acuo/common/model/agreements/TradingParty.java
@@ -1,9 +1,13 @@
 package com.acuo.common.model.agreements;
 
+import lombok.Data;
+
 /**
  * Not to be confused by our ACUO TradingAccount notion : this is the layer between Organisation
  * and LegalEntity
  */
+
+@Data
 public class TradingParty {
 
     private String ampId;
