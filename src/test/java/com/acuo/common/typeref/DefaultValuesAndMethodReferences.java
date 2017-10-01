59c44a2066ebc09ff45e3a9fdf2f5bf272b6085b
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-10-01 16:58:33 +0100

    OW-835 fixed mockito warnings

--- src/test/java/com/acuo/common/typeref/DefaultValuesAndMethodReferences.java
+++ src/test/java/com/acuo/common/typeref/DefaultValuesAndMethodReferences.java
@@ -3,7 +3,7 @@ package com.acuo.common.typeref;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
